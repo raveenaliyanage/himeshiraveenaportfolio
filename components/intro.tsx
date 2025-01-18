d5113e694ca4c65419700d90f84bccbdfba0791d
Raveena Liyanage <144007393+raveenaliyanage@users.noreply.github.com>
2025-01-18 21:17:34 +0530

    JPG

--- components/intro.tsx
+++ components/intro.tsx
@@ -33,7 +33,7 @@ export default function Intro() {
           >
             <Image
               //src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              src="/himeshi.jpg"
+              src="/raveena.JPG"
               //src="https://unsplash.com/photos/4kG65jsR-gc"
               alt="Raveena portrait"
               width="192"
